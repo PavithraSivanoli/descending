f9a2949067bb9847a9d4552b23ca625518489a5a
unknown <pavithrasivanoli@gmail.com>
2022-12-15 21:38:09 +0530

    adding the files

---
+++ js/new.js
@@ -0,0 +1,108 @@
+var a=20;
+var b=30;
+var c=10;
+var d=40;
+
+if ((a>b)&&(a>c)&&(a>d))
+{
+	if ((b>c)&&(b>d))
+	{
+	if (c>d)
+	{
+		console.log(a,b,c,d)
+	}
+	else
+	{
+		console.log(a,b,d,c)
+	}
+}
+}
+
+/*if ((a>b) && (a>c) && (a>d))
+{
+        if ((b>c)&&(b>d))
+        {
+            if(c>d)
+			{
+				console.log(a,b,c,d);
+			}
+			else
+			{
+				console.log(a,b,d,c)
+			}
+        }
+       else
+		{
+			console.log(a,b,d,c);
+		}
+}
+
+		
+if ((b>a) && (b>c) && (b>d))
+{
+        if ((a>c)&& (a>d))
+        {
+            if(c>d)
+			{
+				console.log(b,a,c,d);
+			}
+			else{
+				console.log(b,a,d,c)
+			}
+			
+		}
+		else
+		{
+			console.log(b,a,d,c);
+		}
+
+}
+else
+{
+}
+ if ((c>a) && (c>b) && (c>d))
+{
+        if ((a>b)&&(a>d))
+        {
+           if(b>d)
+			{
+				console.log(c,a,b,d);
+			}
+			else
+			{
+				console.log(c,a,d,b)
+			}
+		}
+			else
+		{
+			console.log(c,a,d,b);
+		}
+
+}
+else
+{
+}
+ if ((d>a) && (d>b) && (d>c))
+{
+        if ((a>b)&&(a>c))
+        {
+         if(b>c)
+			{
+				console.log(d,a,b,c);
+			}
+			else
+			{
+				console.log(d,a,c,b)
+			}
+		}
+		
+			else
+		{
+			console.log(d,a,d,c);
+		}
+
+}
+else
+{
+}
+*/
